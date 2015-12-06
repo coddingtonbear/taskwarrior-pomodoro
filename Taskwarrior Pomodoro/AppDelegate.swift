ae8edc75aa50eef8eaf974a79a47d0c92e9e4569
Adam Coddington <me@adamcoddington.net>
2015-12-05 16:51:06 -0800

    Initial Commit

---
+++ Taskwarrior Pomodoro/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Taskwarrior Pomodoro
+//
+//  Created by Adam Coddington on 12/5/15.
+//  Copyright © 2015 Adam Coddington. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
